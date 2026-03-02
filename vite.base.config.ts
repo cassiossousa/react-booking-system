e413f4d29bb8f5548afbbc5c3fedaae4aeee7a13
Cássio dos Santos Sousa <cassiosantossousa@gmail.com>
2026-03-02 15:24:49 -0300

    chore: trying to fix the build

--- vite.base.config.ts
+++ vite.base.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export const viteBaseConfig = defineConfig({
+export const viteBaseConfig = defineConfig(({ command }) => ({
   plugins: [react()],
+  base: command === 'build' ? '/react-booking-system/' : '/',
   server: {
     port: 5173,
   },
-});
+}));
